5f66eb66a783491167b34359856822a9c8c01d58
HelAsia <asiaheluszka1993@gmail.com>
2018-05-29 10:46:18 +0200

    Sorting menu has been added

---
+++ mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/MainNoRegisteredActivity.java
@@ -0,0 +1,13 @@
+package com.moje.przepisy.mojeprzepisy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainNoRegisteredActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main_no_registered);
+  }
+}
