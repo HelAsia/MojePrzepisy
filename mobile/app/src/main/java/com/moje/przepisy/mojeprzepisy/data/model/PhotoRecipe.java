929d015ed0715713d49115649a5c0a894c71c51a
HelAsia <asiaheluszka1993@gmail.com>
2018-08-07 00:22:47 +0200

    First page to add recipe has been created

--- mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/data/model/PhotoRecipe.java
+++ mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/data/model/PhotoRecipe.java
@@ -1,5 +1,35 @@
 package com.moje.przepisy.mojeprzepisy.data.model;
 
 public class PhotoRecipe {
+  public int photoId;
+  public String photoURL;
+  public String photoImage;
 
+  public PhotoRecipe(){
+
+  }
+
+  public int getPhotoId() {
+    return photoId;
+  }
+
+  public String getPhotoURL() {
+    return photoURL;
+  }
+
+  public String getPhotoImage() {
+    return photoImage;
+  }
+
+  public void setPhotoId(int photoId) {
+    this.photoId = photoId;
+  }
+
+  public void setPhotoURL(String photoURL) {
+    this.photoURL = photoURL;
+  }
+
+  public void setPhotoImage(String photoImage) {
+    this.photoImage = photoImage;
+  }
 }
