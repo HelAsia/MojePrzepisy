54dac598f02cf224e2096c962b13e1b1a95197f3
HelAsia <asiaheluszka1993@gmail.com>
2018-08-06 17:11:53 +0200

    Step (POJO) has been updated

--- mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/data/model/Step.java
+++ mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/data/model/Step.java
@@ -1,5 +1,53 @@
 package com.moje.przepisy.mojeprzepisy.data.model;
 
 public class Step {
+  public int stepId;
+  public int recipeId;
+  public int photoId;
+  public int stepNumber;
+  public String stepDescription;
 
+  public Step(){
+
+  }
+
+  public int getStepId() {
+    return stepId;
+  }
+
+  public int getRecipeId() {
+    return recipeId;
+  }
+
+  public int getPhotoId() {
+    return photoId;
+  }
+
+  public int getStepNumber() {
+    return stepNumber;
+  }
+
+  public String getStepDescription() {
+    return stepDescription;
+  }
+
+  public void setStepId(int stepId) {
+    this.stepId = stepId;
+  }
+
+  public void setRecipeId(int recipeId) {
+    this.recipeId = recipeId;
+  }
+
+  public void setPhotoId(int photoId) {
+    this.photoId = photoId;
+  }
+
+  public void setStepNumber(int stepNumber) {
+    this.stepNumber = stepNumber;
+  }
+
+  public void setStepDescription(String stepDescription) {
+    this.stepDescription = stepDescription;
+  }
 }
