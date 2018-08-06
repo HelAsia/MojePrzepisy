d35f24c03baddadc0e9636a6a6bee10dc3fd7cb1
HelAsia <asiaheluszka1993@gmail.com>
2018-08-06 17:29:10 +0200

    Comment (POJO) has been updated

--- mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/data/model/Comment.java
+++ mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/data/model/Comment.java
@@ -1,5 +1,44 @@
 package com.moje.przepisy.mojeprzepisy.data.model;
 
 public class Comment {
+  public int commentId;
+  public int recipeId;
+  public String comment;
+  public String authorName;
 
+  public Comment(){
+
+  }
+
+  public int getCommentId() {
+    return commentId;
+  }
+
+  public int getRecipeId() {
+    return recipeId;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public String getAuthorName() {
+    return authorName;
+  }
+
+  public void setCommentId(int commentId) {
+    this.commentId = commentId;
+  }
+
+  public void setRecipeId(int recipeId) {
+    this.recipeId = recipeId;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  public void setAuthorName(String authorName) {
+    this.authorName = authorName;
+  }
 }
