a4e22d8ddfda4501d303d4d604f7e65d0ac8a2c7
HelAsia <asiaheluszka1993@gmail.com>
2018-08-11 01:14:51 +0200

    Updated Number picker

--- mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/utils/TimeSetDialog.java
+++ mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/utils/TimeSetDialog.java
@@ -5,14 +5,15 @@ import android.app.Dialog;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 import com.moje.przepisy.mojeprzepisy.R;
 
 public class TimeSetDialog {
-
   String hour;
   String minute;
+  int hourInt;
+  int minuteInt;
 
   public void showDialog(Activity activity, final TextView textViewToSet){
     final Dialog dialog = new Dialog(activity);
@@ -20,31 +21,28 @@ public class TimeSetDialog {
     dialog.setCancelable(false);
     dialog.setContentView(R.layout.time_set_dialog);
 
-    final Spinner setHourSpinner = dialog.findViewById(R.id.hourSpinner);
-    final Spinner setMinuteSpinner = dialog.findViewById(R.id.minuteSpinner);
+    final NumberPicker hourPicker = dialog.findViewById(R.id.hourPicker);
+    final NumberPicker minutePicker = dialog.findViewById(R.id.minutePicker);
+
+    hourPicker.setMinValue(0);
+    hourPicker.setMaxValue(72);
+    hourPicker.setWrapSelectorWheel(true);
+    minutePicker.setMinValue(0);
+    minutePicker.setMaxValue(59);
+    minutePicker.setWrapSelectorWheel(true);
 
     Button dialogButton = (Button) dialog.findViewById(R.id.set_time_button);
     dialogButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        TextView setHourSpinnerTextView = (TextView) setHourSpinner.getSelectedView();
-        hour = setHourSpinnerTextView.getText().toString();
-        TextView setMinuteSpinnerTextView = (TextView) setMinuteSpinner.getSelectedView();
-        minute = setMinuteSpinnerTextView.getText().toString();
+        hourInt = hourPicker.getValue();
+        hour = Integer.toString(hourInt);
+        minuteInt = minutePicker.getValue();
+        minute = Integer.toString(minuteInt);
         dialog.dismiss();
-        textViewToSet.setText(hour+" : "+minute);
-
+        textViewToSet.setText(hour + " : " + minute);
       }
     });
     dialog.show();
   }
-
-  public String getHour(){
-    return hour;
-  }
-
-  public String getMinute(){
-    return minute;
-  }
-
 }
