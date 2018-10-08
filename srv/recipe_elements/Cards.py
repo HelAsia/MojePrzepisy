a5655bc18a89aef070a7f8a4d5642bb49d0189fe
HelAsia <asiaheluszka1993@gmail.com>
2018-10-08 10:56:54 +0200

    getAllCards method was updated

--- srv/recipe_elements/Cards.py
+++ srv/recipe_elements/Cards.py
@@ -16,50 +16,72 @@ class Cards:
     def setDatabase(self,database):
         self.database = database
 
+
+
     def getAllCards(self, userID):
-        if not userID == -1:
-            query = u"SELECT * FROM " \
-                        u"(SELECT	R.recipe_id AS recipeId, R.recipe_name AS recipeName, " \
-                        u"R.recipe_main_picture as recipeMainPicture, R.recipe_created_date_time as Date, " \
-                        u"U.user_login AS authorName, count(URS.favorite) AS favoritesCount, " \
-                        u"ROUND(avg(URS.stars),0) AS starsCount " \
-                        u"FROM recipes AS R " \
-                        u"INNER JOIN users AS U " \
-                        u"ON R.user_id = U.user_id " \
-                        u"INNER JOIN users_recipes_stars AS URS " \
-                        u"ON R.recipe_id = URS.recipe_id " \
-                        u"GROUP BY R.recipe_id) R " \
-                        u"LEFT OUTER JOIN " \
-                        u"(SELECT favorite, recipe_id " \
-                        u"FROM users_recipes_stars " \
-                        u"WHERE user_id = {}) AS URSF " \
-                        u"ON recipeId = URSF.recipe_id; ".format(userID)
+        recipeQuery = u"SELECT recipe_id AS recipeId, recipe_name AS recipeName, user_id AS userId, "\
+        u"recipe_main_picture as recipeMainPicture, recipe_created_date_time as Date "\
+        u"FROM recipes; "
 
-        else:
-            query = u"SELECT R.recipe_id AS recipeId, R.recipe_name AS recipeName, U.user_login AS authorName, " \
-                u"count(URS.favorite) AS favoritesCount, ROUND(avg(URS.stars),0) AS starsCount, " \
-                u"R.recipe_main_picture as recipeMainPicture, R.recipe_created_date_time as Date "\
-                u"FROM recipes AS R "\
-                u"INNER JOIN users AS U "\
-                u"ON R.user_id = U.user_id "\
-                u"INNER JOIN users_recipes_stars AS URS "\
-                u"ON R.recipe_id = URS.recipe_id "\
-                u"GROUP BY R.recipe_id; "
+        userQuery = u"SELECT user_id AS userId, user_login AS authorName "\
+        u"FROM users; "
 
-        queryResult = self.database.query(query)
+        starsCountQuery = u"SELECT recipe_id AS recipeId, ROUND(avg(stars), 0) AS starsCount "\
+        u"FROM users_recipes_stars "\
+        u"WHERE stars > 0 "\
+        u"GROUP BY recipeId; "
 
-        if queryResult:
-            Logger.dbg(queryResult)
-            for queryRow in queryResult:
-                if queryRow['favorite'] is None:
-                    queryRow['favorite'] = False
-                if queryRow['favorite'] == 0:
-                     queryRow['favorite'] = False
-                if queryRow['favorite'] == 1:
-                    queryRow['favorite'] = True
-                Logger.info(u"Zmienilo {}".format(queryRow['favorite']))
-            Logger.dbg(queryResult)
-            return queryResult
+        favoriteCountQuery = u"SELECT recipe_id AS recipeId, count(favorite) AS favoritesCount "\
+        u"FROM users_recipes_stars "\
+        u"where favorite = true "\
+        u"GROUP BY recipeId; "
+
+        favoriteQuery = u"SELECT recipe_id AS recipeId, favorite "\
+        u"FROM users_recipes_stars "\
+        u"where user_id = {}; ".format(userID)
+
+        recipeQueryResult = self.database.query(recipeQuery)
+        userQueryResult = self.database.query(userQuery)
+        starsCountQueryResult = self.database.query(starsCountQuery)
+        favoriteCountQueryResult = self.database.query(favoriteCountQuery)
+        favoriteQueryResult = self.database.query(favoriteQuery)
+
+        mainQueryResult = recipeQueryResult
+
+        if mainQueryResult:
+            for mainQueryRow in mainQueryResult:
+                for userQueryRow in userQueryResult:
+                    if mainQueryRow['userId'] == userQueryRow['userId']:
+                        mainQueryRow['authorName'] = userQueryRow['authorName']
+
+            for mainQueryRow in mainQueryResult:
+                for starsQueryRow in starsCountQueryResult:
+                    if mainQueryRow['recipeId'] == starsQueryRow['recipeId']:
+                        mainQueryRow['starsCount'] = starsQueryRow['starsCount']
+
+            for mainQueryRow in mainQueryResult:
+                for favoriteCountQueryRow in favoriteCountQueryResult:
+                    if mainQueryRow['recipeId'] == favoriteCountQueryRow['recipeId']:
+                        mainQueryRow['favoritesCount'] = favoriteCountQueryRow['favoritesCount']
+
+            if not userID == -1:
+                for mainQueryRow in mainQueryResult:
+                    mainQueryRow['favorite'] = False
+                    for favoriteQueryRow in favoriteQueryResult:
+                        if mainQueryRow['recipeId'] == favoriteQueryRow['recipeId']:
+                            mainQueryRow['favorite'] = favoriteQueryRow['favorite']
+                            if mainQueryRow['favorite'] is None:
+                                mainQueryRow['favorite'] = False
+                            if mainQueryRow['favorite'] == 0:
+                                mainQueryRow['favorite'] = False
+                            if mainQueryRow['favorite'] == 1:
+                                mainQueryRow['favorite'] = True
+            else:
+                for mainQueryRow in mainQueryResult:
+                    mainQueryRow['favorite'] = False
+
+            Logger.dbg(mainQueryResult)
+            return mainQueryResult
         else:
             return {}
 
