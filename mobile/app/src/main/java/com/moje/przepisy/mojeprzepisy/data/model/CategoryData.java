81de10d458bfaabc43db91aaa228182c0341bf48
HelAsia <asiaheluszka1993@gmail.com>
2018-10-30 00:14:34 +0100

    Category option was updated

---
+++ mobile/app/src/main/java/com/moje/przepisy/mojeprzepisy/data/model/CategoryData.java
@@ -0,0 +1,11 @@
+package com.moje.przepisy.mojeprzepisy.data.model;
+
+public class CategoryData {
+  public String text;
+  public int drawableNumber;
+
+  public CategoryData(String text, int drawableNumber){
+    this.text = text;
+    this.drawableNumber = drawableNumber;
+  }
+}
